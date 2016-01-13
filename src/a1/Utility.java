35c781f816513664d26d3165964a9245d42967f7
Yogesh <ybhosale12@gmail.com>
2016-01-13 20:29:29 +0530

    Changed.

--- src/a1/Utility.java
+++ src/a1/Utility.java
@@ -1,8 +1,6 @@
 package a1;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.Random;
 
 public class Utility {
@@ -11,49 +9,56 @@ public class Utility {
 	public static int randbit() {
 		return rand.nextInt(2);
 	}
-	
+
 	public static int getk(int n) {
-		int k=0;
-		int x=1;
-		while(x<n) {
-			x*=2; k++;
+		int k = 0;
+		int x = 1;
+		while (x < n) {
+			x *= 2;
+			k++;
 		}
 		return k;
 	}
-	
+
 	public static int randint(int n) {
-		rand=new Random();
-		int x=n;
-		int k=getk(n);
-		while(x>=n) {
-			int tmp=k;
-			int m=1;
-			x=0;
-			while(tmp>0) {
-				x+=m*randbit();
-				m*=2;
+		rand = new Random();
+		int x = n;
+		int k = getk(n);
+		while (x >= n) {
+			int tmp = k;
+			int m = 1;
+			x = 0;
+			while (tmp > 0) {
+				x += m * randbit();
+				m *= 2;
 				tmp--;
 			}
 		}
-		return x+1;
+		return x + 1;
 	}
-	
-	public static void p2() {
-		PrintWriter pw = null;
-		String fName = "output";
-		try {
-			pw = new PrintWriter(new File(fName + ".out"));
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
+
+	public static ArrayList<Integer> p2() {
+		ArrayList<Integer> res = new ArrayList();
+		int cnt = 1000000;
+		while (cnt-- > 0) {
+			res.add(randint(8));
 		}
-		int cnt=1000000;
-		while(cnt-->0) {
-			pw.print(randint(8)+"  ");
+		return res;
+	}
+
+	public static int streak(ArrayList<Integer>a) {
+		int res=1;
+		int cur=1;
+		for(int i=1;i<a.size();i++) {
+			if(a.get(i)==a.get(i-1))
+				cur++;
+			else cur=1;
+			res=Math.max(res,cur);
 		}
-		pw.close();
+		return res;
 	}
 	
 	public static void main(String[] args) {
-		p2();
+		System.out.println(streak(p2()));
 	}
 }
